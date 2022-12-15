c83c02e6181b22aa28bb3bec40e85a82197e7a83
Onur Eredo <onureredo@yahoo.com>
2022-12-15 10:03:48 +0100

    added axios

---
+++ src/Components/Axios.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import axios from 'axios';
+
+
+function FetchAxios() {
+    
+    const [content, setContent] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const fetchData = () => {
+    axios.get(`https://api.quotable.io/random`)
+    .then(res => {
+      // console.log(res) //res.data.content
+
+
+      setContent(res.data.content)
+      setAuthor(res.data.author)
+
+
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+  
+  return (
+
+    <div>
+        <h1>{author}</h1>
+        <h3>{content}</h3>
+        <button onClick={fetchData}>Fetch Data</button>
+    </div>
+  )
+}
+
+export default FetchAxios
