5de5813de8b71d8d262453086b8e87f1280d12c5
Onur Eredo <onureredo@yahoo.com>
2022-12-15 10:54:22 +0100

    final

--- src/Components/Axios.js
+++ src/Components/Axios.js
@@ -10,21 +10,16 @@ function FetchAxios() {
   const fetchData = () => {
     axios.get(`https://api.quotable.io/random`)
     .then(res => {
-      // console.log(res) //res.data.content
-
-
+      // console.log(res.data.content)
       setContent(res.data.content)
       setAuthor(res.data.author)
-
-
     }).catch(err => {
       console.log(err)
     })
   }
   
   return (
-
-    <div>
+    <div className='green_line'>
         <h1>{author}</h1>
         <h3>{content}</h3>
         <button onClick={fetchData}>Fetch Data</button>
